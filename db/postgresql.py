1ba13c3d64bc73e07bca82d68529e317f2e1f717
Otabek Qosimov <otabek.qosimov0207@gmail.com>
2024-10-08 20:48:11 +0300

    Update postgresql.py

--- db/postgresql.py
+++ db/postgresql.py
@@ -40,7 +40,7 @@ class Database:
 
     async def create_table_users(self):
         sql = """
-        CREATE TABLE IF NOT EXISTS "User" (
+        CREATE TABLE IF NOT EXISTS "bot_admin_user" (
         id BIGSERIAL PRIMARY KEY,
         telegram_id BIGINT NOT NULL UNIQUE,
         username VARCHAR(255) DEFAULT NULL
@@ -50,7 +50,7 @@ class Database:
 
     async def create_table_movies(self):
         sql = """
-        CREATE TABLE IF NOT EXISTS "Movie" (
+        CREATE TABLE IF NOT EXISTS "bot_admin_movie" (
         id BIGSERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         year INT NOT NULL,
@@ -65,7 +65,7 @@ class Database:
 
     async def create_table_channels(self):
         sql = """
-        CREATE TABLE IF NOT EXISTS "Channel" (
+        CREATE TABLE IF NOT EXISTS "bot_admin_channel" (
         channel_id VARCHAR(100) PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         url TEXT DEFAULT NULL
@@ -75,51 +75,51 @@ class Database:
 
     # Foydalanuvchilarni olish
     async def select_all_users(self) -> List[Dict[str, Any]]:
-        sql = "SELECT * FROM \"User\";"
+        sql = "SELECT * FROM \"bot_admin_user\";"
         return await self.execute(sql, fetch=True)
 
     # Kanallarni olish
     async def select_all_channels(self) -> List[Dict[str, Any]]:
-        sql = "SELECT * FROM \"Channel\";"
+        sql = "SELECT * FROM \"bot_admin_channel\";"
         return await self.execute(sql, fetch=True)
 
     # Filmlarni olish
     async def select_all_movies(self) -> List[Dict[str, Any]]:
-        sql = "SELECT * FROM \"Movie\";"
+        sql = "SELECT * FROM \"bot_admin_movie\";"
         return await self.execute(sql, fetch=True)
 
     # Foydalanuvchini qo'shish
     async def add_user(self, telegram_id: int, username: str) -> None:
-        sql = "INSERT INTO \"User\" (telegram_id, username) VALUES ($1, $2);"
+        sql = "INSERT INTO \"bot_admin_user\" (telegram_id, username) VALUES ($1, $2);"
         await self.execute(sql, telegram_id, username, execute=True)
 
     # Kanal qo'shish
     async def add_channel(self, channel_id: str, name: str, url: str) -> None:
-        sql = "INSERT INTO \"Channel\" (channel_id, name, url) VALUES ($1, $2, $3);"
+        sql = "INSERT INTO \"bot_admin_channel\" (channel_id, name, url) VALUES ($1, $2, $3);"
         await self.execute(sql, channel_id, name, url, execute=True)
 
     # Film qo'shish
     async def add_movie(self, title: str, year: int, genre: str, language: str, code: str, video_file_id: str) -> None:
-        sql = "INSERT INTO \"Movie\" (title, year, genre, language, code, video_file_id) VALUES ($1, $2, $3, $4, $5, $6);"
+        sql = "INSERT INTO \"bot_admin_movie\" (title, year, genre, language, code, video_file_id) VALUES ($1, $2, $3, $4, $5, $6);"
         await self.execute(sql, title, year, genre,language, code, video_file_id, execute=True)
 
     # Foydalanuvchini o'chirish
     async def delete_user(self, user_id: int) -> None:
-        sql = "DELETE FROM \"User\" WHERE id = $1;"
+        sql = "DELETE FROM \"bot_admin_user\" WHERE id = $1;"
         await self.execute(sql, user_id, execute=True)
 
     # Kanalni o'chirish
     async def delete_channel(self, channel_id: str) -> None:
-        sql = "DELETE FROM \"Channel\" WHERE channel_id = $1;"
+        sql = "DELETE FROM \"bot_admin_channel\" WHERE channel_id = $1;"
         await self.execute(sql, channel_id, execute=True)
 
     # Filmlarni o'chirish
     async def delete_movie(self, movie_id: int) -> None:
-        sql = "DELETE FROM \"Movie\" WHERE id = $1;"
+        sql = "DELETE FROM \"bot_admin_movie\" WHERE id = $1;"
         await self.execute(sql, movie_id, execute=True)
 
     async def select_by_code_movie(self, code: str) -> Dict[str, Any]:
-        sql = "SELECT * FROM \"Movie\" WHERE code = $1;"
+        sql = "SELECT * FROM \"bot_admin_movie\" WHERE code = $1;"
         result = await self.execute(sql, code, fetch=True)
         # Return the first result or None if not found
         return result[0] if result else None
